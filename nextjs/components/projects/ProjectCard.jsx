2adcd37be99ed106fd04b11ae970732679560d65
Paul Bodner <bodner80@gmail.com>
2023-11-02 22:37:03 -0700

    added a flip to project cards

--- nextjs/components/projects/ProjectCard.jsx
+++ nextjs/components/projects/ProjectCard.jsx
@@ -1,27 +1,86 @@
+'use client'
 import Image from "next/image";
-import defaultImage from '@/public/images/default.jpeg'
+import defaultImage from '@/public/images/default.jpeg';
+import { useState, useRef, useEffect } from "react";
+import { motion, useCycle } from "framer-motion";
 
 export default function ProjectCard({styles, project}){
+  const [isFlipped, toggleIsFlipped] = useCycle(false, true);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const cardRef = useRef(null);
 
+  const handleFlip = () => {
+    console.log(cardRef.current.offsetLeft);
+    console.log(cardRef.current.offsetRight);
+    if (!isAnimating){
+      toggleIsFlipped();
+      setIsAnimating(true);
+    }
+  }
+
+  useRef(()=>{
+    
+  })
 
   return (
-    <article className="relative w-48 h-72 m-4 flex flex-col items-center rounded-lg overflow-hidden">
-      <div className="absolute w-full h-full">
-        <Image 
-          src={defaultImage}
-          fill
-          alt={ project.title }
-          style={{
-            objectFit: 'cover',
-            objectPosition: 'top',
-          }}
-          sizes='100px'
-        />
-      </div>
-
-      <div className="absolute w-full bottom-0 pb-4 flex flex-col items-center">
+    <motion.article ref={cardRef} onClick={(e)=>{handleFlip(e)}} className={[styles.projectCard, "relative w-52 h-72"].join(' ')}
+      // initial={ false }
+      // animate={{ paddingBottom: isFlipped ? '2em': 0 }}
+    >
+      <div className="absolute w-full bottom-0 flex flex-col items-center">
         <h2>{ project.title }</h2>
       </div>
-    </article>
+
+      <motion.div className={[styles.inner, "relative w-full h-full bottom-0"].join(' ')}
+        initial={false}
+        animate={{ rotateY: isFlipped? 180 : 360 }}
+        transition={{ duration: 0.3, animationDirection: 'normal' }}
+        onAnimationComplete={()=> setIsAnimating(false) }
+      >
+
+        <div className={[styles.front, "w-full h-full"]}>
+          <Image 
+            src={defaultImage}
+            fill
+            alt={ project.title }
+            style={{
+              objectFit: 'cover',
+              objectPosition: 'top',
+            }}
+            sizes='100px'
+            />
+        </div>
+
+        <div className={[styles.back, "relative top-0 w-full h-full overflow-hidden"].join(' ')}>
+          <div className={[styles.background, "absolute w-[100vw] h-[100vh]"].join(' ')}
+            initial={false}
+            style={{
+              top: isFlipped ? `-${cardRef.current.offsetTop}px` : 0,
+              left: isFlipped ? `-${cardRef.current.offsetLeft}px` : 0,
+            }}
+          >
+          </div>
+          <p>{project.description}</p>
+          <div>
+            <h4>Stack</h4>
+            <ul>
+              <li> Python
+                <ul>
+                  <li>Django</li>
+                  <li>Django-Rest-Framework</li>
+                </ul>
+              </li>
+              <li>JavaScript
+                <ul>
+                  <li>Next.js</li>
+                  <li>Framer Motion</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </motion.div>
+    </motion.article>
   )
 }

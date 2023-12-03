cefced1658b895ed39177bd15ed2e4648c806c73
Paul Bodner <bodner80@gmail.com>
2023-12-02 17:29:31 -0800

    added icons for Languages, Technologies and Frameworks

--- django/pb_backend/languages/models.py
+++ django/pb_backend/languages/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Language(models.Model):
   name=models.CharField(max_length=100, blank=True, null=True, unique=True)
+  image=models.ImageField(upload_to='images/icons/', null=True, blank=True)
 
   def __str__(self):
     return self.name
